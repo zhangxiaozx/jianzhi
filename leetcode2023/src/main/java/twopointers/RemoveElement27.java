4c4b9fca8405e7611614940196ed6fe3817f81ec
zhangxiaozx <22894307896@qq.com>
2023-07-23 14:33:47 +0800

    test

--- leetcode2023/src/main/java/twopointers/RemoveElement27.java
+++ leetcode2023/src/main/java/twopointers/RemoveElement27.java
@@ -1,4 +1,28 @@
 package twopointers;
 
+import java.util.Arrays;
+
 public class RemoveElement27 {
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        String[] names = new String[10];
+        for (String a : names) {
+            a = " ";
+
+        }
+        System.out.println(Arrays.asList(names));
+
+    }
 }
